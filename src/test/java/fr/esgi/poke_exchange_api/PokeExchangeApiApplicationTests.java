cc752c59c45b3c76449a93d69112292c1be884c9
Antonin Texier <38111887+chukitipok@users.noreply.github.com>
2021-05-10 02:26:09 +0200

    Base architecture - User API - JWT authentication
    * config(packages): Rename packages and artifactId.
    
    * config(packages): Rename files.
    
    * feat(infrastructure): Implement user entity class.
    
    * feat(infrastructure): Implement user repository class.
    
    * refactor(infrastructure): Rename rank field by eloPoints.
    
    * feat(domain): Implement user domain models.
    
    * feat(domain): Implement auth domain model request.
    
    * feat(domain): Implement user domain exception.
    
    * feat(domain): Implement user domain mappers.
    
    * feat(domain): Implement user domain service.
    
    * feat(exposition): Implement user rest controller class.
    
    * feat(security): Implement jwt authentication entry point class.
    
    * feat(security): Implement jwt token util class.
    
    * feat(security): Implement jwt user details service class.
    
    * feat(security): Implement jwt request filter class.
    
    * feat(security): Implement web security config class.
    
    * feat(auth): Implement login request body class.
    
    * feat(auth): Implement login request response class.
    
    * feat(auth): Implement properties length rules class.
    
    * feat(auth): Implement register request body validator class.
    
    * feat(auth): Implement authentication controller class.
    
    * feat(auth): Implement authentication controller class.
    
    * feat(auth): Implement authentication controller class.

--- src/test/java/fr/esgi/poke_exchange_api/PokeExchangeApiApplicationTests.java
+++ src/test/java/fr/esgi/poke_exchange_api/PokeExchangeApiApplicationTests.java
@@ -1,10 +1,10 @@
-package fr.esgi.pokExchange;
+package fr.esgi.poke_exchange_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PokExchangeApplicationTests {
+class PokeExchangeApiApplicationTests {
 
 	@Test
 	void contextLoads() {
